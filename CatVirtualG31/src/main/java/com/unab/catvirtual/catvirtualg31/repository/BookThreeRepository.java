0a75d8301fd283c662be68960b60b5dfa6dcc20b
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-12-13 23:22:58 -0500

    Delete CatVirtualG31 directory

--- CatVirtualG31/src/main/java/com/unab/catvirtual/catvirtualg31/repository/BookThreeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unab.catvirtual.catvirtualg31.repository;
-
-import com.unab.catvirtual.catvirtualg31.entity.BookThree;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookThreeRepository extends MongoRepository<BookThree,String> {
-}
