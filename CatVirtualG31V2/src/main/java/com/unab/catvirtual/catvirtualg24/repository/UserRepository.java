83e636bee964c15e9c815a03848c11418172d5bd
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-12-19 14:41:57 -0500

    Delete CatVirtualG31V2 directory

--- CatVirtualG31V2/src/main/java/com/unab/catvirtual/catvirtualg24/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unab.catvirtual.catvirtualg24.repository;
-
-import com.unab.catvirtual.catvirtualg24.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-}
