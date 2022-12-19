4ce53b8219c360299365e998a212e748d8c05657
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-12-19 14:42:08 -0500

    Delete CatVirtualG31V3 directory

--- CatVirtualG31V3/src/main/java/com/unab/catvirtual/catvirtualg24/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unab.catvirtual.catvirtualg24.repository;
-
-import com.unab.catvirtual.catvirtualg24.entity.Author;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuthorRepository extends MongoRepository<Author,String> {
-}
--- CatVirtualG31V3/src/main/java/com/unab/catvirtual/catvirtualg24/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unab.catvirtual.catvirtualg24.repository;
-
-import com.unab.catvirtual.catvirtualg24.entity.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoryRepository extends MongoRepository<Category,String> {
-}
